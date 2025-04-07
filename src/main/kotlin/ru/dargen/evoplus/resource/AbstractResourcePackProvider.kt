067d532f8227a575f28a3b3c68daeb4f02e66c82
dargen <dargencode@mail.ru>
2025-04-07 20:37:39 +0300

    daemon and android

--- src/main/kotlin/ru/dargen/evoplus/resource/AbstractResourcePackProvider.kt
+++ src/main/kotlin/ru/dargen/evoplus/resource/AbstractResourcePackProvider.kt
@@ -2,7 +2,7 @@ package ru.dargen.evoplus.resource
 
 import net.minecraft.resource.*
 import net.minecraft.resource.ResourcePackProfile.InsertionPosition
-import net.minecraft.resource.ResourcePackProfile.Metadata
+import net.minecraft.resource.featuretoggle.FeatureSet
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import java.util.*
@@ -12,19 +12,20 @@ abstract class AbstractResourcePackProvider(
     val id: String, val name: String, description: String,
 ) : ResourcePackProvider {
 
-    private val metadata = ResourcePackInfo(
+    private val info = ResourcePackInfo(
         id, Text.of(name), Source, Optional.empty()
     )
+    private val metadata = ResourcePackProfile.Metadata(
+        Text.of(description), ResourcePackCompatibility.COMPATIBLE,
+        FeatureSet.empty(), emptyList()
+    )
     protected open val pack
-        get() = ResourcePackProfile.create(
-            metadata, object : ResourcePackProfile.PackFactory {
+        get() = ResourcePackProfile(
+            info, object : ResourcePackProfile.PackFactory {
                 override fun open(info: ResourcePackInfo) = openPack(info)
-
-                override fun openWithOverlays(info: ResourcePackInfo,metadata: Metadata) = openPack(info)
-
-            },
-            ResourceType.CLIENT_RESOURCES, ResourcePackPosition(true, InsertionPosition.TOP, true)
-        )!!
+                override fun openWithOverlays(info: ResourcePackInfo, metadata: ResourcePackProfile.Metadata) = openPack(info)
+            }, metadata, ResourcePackPosition(true, InsertionPosition.TOP, true)
+        )
 
     override fun register(profileAdder: Consumer<ResourcePackProfile>) = profileAdder.accept(pack)
 
--- src/main/kotlin/ru/dargen/evoplus/resource/builtin/EvoPlusPackProvider.kt
+++ src/main/kotlin/ru/dargen/evoplus/resource/builtin/EvoPlusPackProvider.kt
@@ -9,8 +9,6 @@ import ru.dargen.evoplus.resource.AbstractResourcePackProvider
 
 class EvoPlusPackProvider : AbstractResourcePackProvider("evo-plus", "EvoPlus", "Mod additional pack") {
 
-    override val pack = super.pack
-
     override fun openPack(info: ResourcePackInfo): ResourcePack {
         return if (EvoPlus.DevEnvironment) DirectoryResourcePack.DirectoryBackedFactory(EvoPlus.Path).open(info)
         else ZipResourcePack.ZipBackedFactory(EvoPlus.Path.toFile()).open(info)
